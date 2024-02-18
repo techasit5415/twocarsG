56cdef06bd0e23ba256f769a970b46d7ad8f1e3c
techasit vanit <techasit2424@gmail.com>
2024-02-18 23:31:57 +0700

    gu commit all ar i sus

--- button.java
+++ button.java
@@ -4,22 +4,19 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class button extends JFrame {
     public static void main(String[] args) {
-        ButtonOnStart();
+       // ButtonOnStart();
         buttondead();
        
     }
     
     public static void ButtonOnStart(){
-        JFrame frame = new JFrame("Button Example");
+        JFrame frame = new JFrame("TWO Cars");
         JButton btn=new JButton("");  
         btn.setBounds(250,470,300,100);  
         frame.add(btn);  
@@ -57,32 +54,36 @@ public class button extends JFrame {
     }
 
     public static void buttondead(){
-        JFrame frame = new JFrame("Button Example");
-        JButton btnsrt=new JButton("Restart");  
-        btnsrt.setBounds(250,200,300,100);  
-        frame.add(btnsrt);  
+        JFrame frame = new JFrame("Two Cars");
+        JButton btn_rst=new JButton("Restart");  
+        btn_rst.setBounds(250,200,300,100);  
+        frame.add(btn_rst);  
         frame.setSize(400,400);  
         frame.setLayout(null);  
-        frame.setVisible(true);  
+        frame.setVisible(true);
+        
+        
         //dont show background btn
-        // btn.setOpaque(false);
-        // btn.setContentAreaFilled(false);
+
+        // btn_rst.setOpaque(false);
+        // btn_rst.setContentAreaFilled(false);
         // btn.setBorderPainted(false); 
         //
-        // btn.addActionListener(new ActionListener() {
-        //     // @Override
-        //     // public void actionPerformed(ActionEvent e) {
-        //     //         frame.dispose();
-        //     //         CarGameGui c1 = new CarGameGui();
-        //     // }
-        // });
 
-
+        //it about restart
+        btn_rst.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    frame.dispose();
+                    CarGameGui c1 = new CarGameGui();
+            }
+        });
+    
 
       
-        JButton btnq=new JButton("QUIT");  
-        btnq.setBounds(250,500,300,100);  
-        frame.add(btnq);  
+        JButton btn_q=new JButton("QUIT");  
+        btn_q.setBounds(250,500,300,100);  
+        frame.add(btn_q);  
         frame.setSize(400,400);  
         frame.setLayout(null);  
         frame.setVisible(true);  
@@ -92,16 +93,8 @@ public class button extends JFrame {
         // btn1.setBorderPainted(false); 
         //
 
-        //it about restart
-        btnsrt.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    frame.dispose();
-                    CarGameGui c1 = new CarGameGui();
-            }
-        });
 
-        btnq.addActionListener(new ActionListener() {
+        btn_q.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                     frame.dispose();
@@ -110,7 +103,8 @@ public class button extends JFrame {
         });
 
 
-
+        
+        //it about background
         ImageIcon originalBackgroundIcon = new ImageIcon("img/menu.png");
         // Resize the background ImageIcon to fit the preferred size of the frame
         Image backgroundImg = originalBackgroundIcon.getImage().getScaledInstance(800, 900, Image.SCALE_SMOOTH);
