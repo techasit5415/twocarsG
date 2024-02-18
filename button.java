33a15e27a1aa471ef66c73193f009785e280433f
techasit vanit <techasit2424@gmail.com>
2024-02-18 20:21:01 +0700

    kuy button

--- button.java
+++ button.java
@@ -3,64 +3,65 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class button extends CarGameGui {
+public class button extends JFrame {
     public static void main(String[] args) {
         ButtonOnStart();
-        
     }
-    public static void ButtonOnStart(){
-        // fr = new JFrame("Two Cars");
-        // fr.setLayout(null);
-        // background = new JLabel("", new ImageIcon("img/giffy2.gif"), JLabel.CENTER);
-        // background.setBounds(0, 0, WIDTH, HEIGHT);
-        // fr.add(background);
-        // background.add(carPanel);
-        // background.add(RcarPanel);
-        // fr.setSize(WIDTH, HEIGHT);
-        // fr.setVisible(true);
-        // fr.setResizable(false);
-        // fr.setLocationRelativeTo(null);
-        // fr.addKeyListener(hnd);
-        // fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    public static void ButtonOnStart() {
+        JFrame frame = new JFrame("Button Example");
 
-        
-     
 
+        // Create a JPanel to hold the button
+        JPanel panel = new JPanel();
         // Create an ImageIcon
         ImageIcon icon = new ImageIcon("img/startbutton.png");
         // Create a JButton
+
+        
+
         JButton button = new JButton(icon);
-
-
         button.setPreferredSize(new Dimension(700, 800));
         button.setOpaque(false);
         button.setContentAreaFilled(false);
         button.setBorderPainted(false);
 
-
+        JLabel background;
+        background = new JLabel("", new ImageIcon("img/giffy2.gif"), JLabel.CENTER);
+        background.setBounds(0, 0, 800, 900);
+        
+        
         // Add ActionListener to the button
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    fr.dispose();
+                    // Dispose the current frame
+                    frame.dispose();
+                    // Create a new instance of CarGameGui
                     CarGameGui c1 = new CarGameGui();
-                    
                 } catch (Exception ex) {
-                    // TODO: handle exception
+                    // Handle exception
+                    ex.printStackTrace();
                 }
             }
         });
         
         // Add the button to the panel
+        
         panel.add(button);
+        
         
         // Add the panel to the frame
         frame.add(panel);
+        
+        
         
         // Set frame properties
         frame.setSize(800, 900);
