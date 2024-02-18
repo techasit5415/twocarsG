c97d3a4cd8694fe936bf7f188047e3ed2b9078f7
techasit vanit <techasit2424@gmail.com>
2024-02-18 17:06:50 +0700

    edit

--- CarGameGui.java
+++ CarGameGui.java
@@ -1,12 +1,10 @@
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 import javax.swing.ImageIcon;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -38,7 +36,6 @@ public class CarGameGui {
     private void initGUI() {
         fr = new JFrame("Two Cars");
         fr.setLayout(null);
-
         background = new JLabel("", new ImageIcon("img/giffy2.gif"), JLabel.CENTER);
         background.setBounds(0, 0, WIDTH, HEIGHT);
         fr.add(background);
@@ -75,24 +72,8 @@ public class CarGameGui {
 
     private void addObstruction() {
         Random random = new Random();
-
+    
         int rand = (int) ((Math.random() * 10) + 1);
-<<<<<<< HEAD
-
-        List<Integer> usedXPositions = new ArrayList<>(); // Keep track of used X positions
-
-        for (int i = 0; i < rand; i++) {
-            int randomObX = getRandomXPosition(usedXPositions);
-            int randomPX = getRandomXPosition(usedXPositions);
-
-            Obstacle obstacle = new Obstacle(this, ObSpeed, randomObX, 70, scores);
-            Point point = new Point(this, ObSpeed, randomPX, 70, scores); // Pass scores to Point
-            obstacle.start();
-            point.start();
-
-            usedXPositions.add(randomObX);
-            usedXPositions.add(randomPX);
-=======
     
         List<Integer> usedXPositions = new ArrayList<>();
     
@@ -102,39 +83,16 @@ public class CarGameGui {
     
         for (int i = 0; i < rand; i++) {
             createObstacleAndPoint(usedXPositions, rate);
->>>>>>> f0f85ca18cbb34a49e697de5c79f6730d0813fd5
 
             try {
                 Thread.sleep(random.nextInt(500) + frequency);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-<<<<<<< HEAD
-        }
-
-        try {
-            Thread.sleep(random.nextInt(500) + 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-=======
->>>>>>> f0f85ca18cbb34a49e697de5c79f6730d0813fd5
         }
     
         addObstruction();
     }
-<<<<<<< HEAD
-
-    private int getRandomXPosition(List<Integer> usedXPositions) {
-        int rand;
-        do {
-            rand = (int) ((Math.random() * 4));
-        } while (usedXPositions.contains(rand));
-        int[] xPositions = { 260, 360, 470, 570 };
-        return xPositions[rand];
-    }
-
-
-=======
     
     private void createObstacleAndPoint(List<Integer> usedXPositions, int rate) {
         int randomObX = getRandomXPosition(usedXPositions);
@@ -159,5 +117,4 @@ public class CarGameGui {
         return xPositions[rand];
     }    
     
->>>>>>> f0f85ca18cbb34a49e697de5c79f6730d0813fd5
 }
